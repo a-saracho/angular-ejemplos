3491032979fd54b54ce62a4f0d80ff1be99dfa6f
Ainara Saracho <ainara.saracho@gmail.com>
2021-04-06 19:57:28 +0200

    Localización y locales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEs);
+
 @NgModule({
   declarations: [
     AppComponent,
